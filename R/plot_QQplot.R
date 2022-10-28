2d716db56bc54bd067dd04b62582a347793b68fc
Joanna Żyła <joanna.zyla@polsl.pl>
2022-10-28 10:04:28 +0200

    Documentation and dataset update

--- R/plot_QQplot.R
+++ R/plot_QQplot.R
@@ -1,3 +1,23 @@
+#' QQplot for GMM decomposition
+#'
+#' Function return ggplot object with fit diagnostic Quantile-Quantile plot for one normal distribution and fitted GMM.
+#' The QQplot is also return as regular output of runGMM.
+#'
+#' @param data Vector of data
+#' @param GModel List of GModel parameters i.e GModel$model$alpha, GModel$model$mu, GModel$model$sigma. Given as output of function gaussian_mixture_vector.
+#'
+#' @importFrom ggpubr ggarrange
+#' @importFrom ggplot2 ggplot aes aes_string geom_smooth geom_point ylab xlab ggtitle theme
+#' @importFrom ggplot2 stat_qq stat_qq_lin
+#' @importFrom stats qqplot
+#'
+#' @examples
+#' data<-generate_norm1D(1000, alpha=c(0.2,0.4,0.4), mu=c(-15,0,15), sigma=c(1,2,3))
+#' GModel<-list(model=list(alpha=c(0.2,0.4,0.4), mu=c(-15,0,15), sigma=c(1,2,3)))
+#' plot_QQplot(data,GModel)
+#' @seealso \code{\link{runGMM}} and \code{\link{gaussian_mixture_vector}}
+#'
+#' @export
 plot_QQplot<-function(data,GModel){
 tor<-generate_norm1D(length(data), GModel$model$alpha, GModel$model$mu, GModel$model$sigma)
 quants<-qqplot(data,tor$Dist,plot.it = F)
