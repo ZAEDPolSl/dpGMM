7d0da7494343b2d7c0df8a1ff83f49f8ae342182
KamilaSzumala <kamila.szumala2000@gmail.com>
2023-03-25 19:32:00 +0100

    opts for 1D GMM - .R file

---
+++ R/GMM_1D_opts.R
@@ -0,0 +1,39 @@
+#' Default configuration for 1D Gaussian Mixture decomposition
+#'
+#' A list with parameters customizing a GMM for 1D and binned data. Each component of the
+#' list is an effective argument for \code{\link{runGMM}}.
+#'
+#' @param eps_change Criterion for early stopping of EM (1e-7, by default) given by the following formula:
+#' \deqn{\sum{(|\alpha - \alpha_{old})|} + \frac{\sum{(\frac{|\sigma^2 - \sigma^2_{old}|}{\sigma^2})}}{length(\alpha)}}
+#' @param max_iter Maximum number of iterations of EM algorithm. By default it is \code{max_iter = 50 000}.
+#' @param SW Parameter for calculating minimum variance of each Gaussian component (0.01, by default) using the following formula:
+#' \deqn{(\frac{SW*range(x)}{no.of.components)})^2}. Lower value means smaller component variance allowed.
+#' @param IC Information criterion used to select the number of model components.
+#' Possible methods are "AIC","AICc", "BIC" (default), "ICL-BIC" or "LR".
+#' @param sigmas.dev Parameter used to define close GMM components that needs to be merged. For each component, standard deviation is multiplied by \code{sigmas.dev} to estimate the distance from component mean.
+#' All other components within this distance are merged. By default it is \code{sigmas.dev = 2.5}. When \code{sigmas.dev = 0} no components are merged.
+#' @param quick_stop Logical value. Determines if stop searching of the number of components earlier based on the Likelihood Ratio Test. Used to speed up the function (TRUE, by default).
+#' @param signi Significance level set for Likelihood Ratio Test (0.05, by default).
+#' @param fixed Logical value. Fit GMM for selected number of components given by KS (FALSE, by default).
+#'
+#' @examples
+#' # display all default settings
+#' GMM_1D_opts
+#'
+#' # create a new settings object
+#' custom.settings <- GMM_1D_opts
+#' custom.settings$IC <- "AIC"
+#' custom.settings
+#'
+#' @export
+GMM_1D_opts <- list(
+  eps_change = 1e-7,
+  max_iter = 50000,
+  SW = 0.01,
+  IC = 'BIC',
+  sigmas.dev = 2.5,
+  quick_stop = FALSE,
+  signi = 0.05,
+  fixed = FALSE
+)
+class(GMM_1D_opts) <- "gmm2_opts"
